c5b99867ac321f28a7a389265f9e9118b38e85cf
kosmo_11 <rana3782@gmail.com>
2019-07-27 15:03:24 +0900

    //

--- src/components/AddPlayerForm.js
+++ src/components/AddPlayerForm.js
@@ -1,36 +1,41 @@
 import React from 'react';
 
 export class AddPlayerForm extends React.Component {
+	// state = {
+	// 	value: ''
+	// }
 
-  	state = {
-  	  value : ''
+	// handleValueChange = (e) => {
+	// 	console.log(e);
+	// 	this.setState({value: e.target.value})
+	// }
+	textInput = React.createRef();
+
+	handleSubmit = (e) => {
+		console.log(e);
+		// 화면을 리프레쉬하는 기본 이벤트 막기
+		e.preventDefault();
+
+		// 폼체크, 입력폼 체크
+		const playerInput =document.getElementById("player");
+		console.log(playerInput.validity.valid);
+		console.log('form valid: ', document.getElementById("form").checkValidity());
+
+		if (!playerInput.validity.valid) {
+			// 에러메시지 보여주기
+			return;
+		}
+
+		// this.props.addPlayer(this.state.value);
+		// this.textInput.current 가 Dom 노드에 해당
+		this.props.addPlayer(this.textInput.current.value);
 	}
 
-	handleValueChange = (e) =>{
-  	  //console.log(e);
-  	  this.setState({value:e.target.value})
-	}
-
-  handleSubmit = (e) =>{
-	//화명을 리플레쉬하는 기본 이벤트 막기
-	e.preventDefault();
-	//폼체크 , 입력폼 체크
-	const playInput = document.getElementById("player");
-	console.log(playInput.validity.valid);
-	console.log('formcheck->',document.getElementById("form").checkValidity());
-
-	if(!playInput.validity.valid){
-	  //에러메시지보이기
-	  return;
-	}
-
-	this.props.addPlayer(this.state.value);
-
-  }
 	render() {
 		return (
 			<form className="form" onSubmit={this.handleSubmit} noValidate id="form">
-				<input className="input" type="text" placeholder="enter a player's name" value={this.state.value} onChange={this.handleValueChange} required  id="player"></input>
+				<input className="input" type="text" placeholder="enter a player's name"
+					required id="player" ref={this.textInput}></input>
 				<input className="input" type="submit" value="Add Player"></input>
 			</form>
 		);
