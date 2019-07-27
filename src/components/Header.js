e6acdf794d408cfa8ffcba3ca7a8bfa9b84e66c2
kosmo_11 <rana3782@gmail.com>
2019-07-27 14:48:28 +0900

    //

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,13 @@ import {Stopwatch} from "./Stopwatch";
 // 앞글자는 대문자, 반드시 react element를 리턴
 export const Header = ({title, players}) => {
 	// 객체 해체 할당
+	// const {title, players} = props;
 
 	return (
 		<header className="header">
 			<Statistics players={players} />
 			<h1 className="h1">{title}</h1>
-		  	<Stopwatch></Stopwatch>
+			<Stopwatch/>
 		</header>
 	)
 }
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import {Counter} from "./Counter";
 
-export const Player = ({name, score, removePlayer, id, changeScore}) => {
-	return (
-		<div className="player">
+export class Player extends React.Component {
+	render() {
+		const {name, score, removePlayer, id, changeScore} = this.props;
+
+		console.log(name, 'rendered');
+
+		return	(
+			<div className="player">
 			<span className="player-name">
 				<button className="remove-player" onClick={() => removePlayer(id)}>x</button>
 				{name}
 			</span>
-			<Counter id={id} score={score} changeScore={changeScore} />
-		</div>
-	);
+				<Counter id={id} score={score} changeScore={changeScore} />
+			</div>
+		);
+	}
+
+	componentWillReceiveProps(nextProps, nextContext) {
+	  //console.log('componentWillReceiveProps', nextProps);
+	}
+
+	shouldComponentUpdate(nextProps, nextState, nextContext) {
+	  //console.log('shouldComponentUpdate', nextProps);
+	  return nextProps.score !== this.props.score ? true:false;
+	}
 }
